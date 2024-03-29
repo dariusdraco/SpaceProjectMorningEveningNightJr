2c8630cec7040f56538d2264f6d8d14180d12a23
dariusdraco <95847901+dariusdraco@users.noreply.github.com>
2024-03-29 11:54:16 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function game(){
+    window.location.href="game.html";
+}
+function reading(){
+    window.location.href="reading.html";
+}
+function fact(){
+    window.location.href="fact.html";
+}
